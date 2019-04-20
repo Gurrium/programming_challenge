e82f4a750f97142fc41c90e7cd03e82b6a236fdd
taira120 <taira1999120@gmail.com>
2019-04-21 05:02:39 +0900

    fix :skul:

--- 2/11340.c
+++ 2/11340.c
@@ -12,27 +12,25 @@ int main() {
   size_t bufsize = 10000;
   line = (char *)malloc(bufsize * sizeof(char));
 
-  for(i = 0;i < '~';i++)
-    cost[i] = 0;
-
   scanf("%d", &n);
-  // while(1) {
-  for(i = 0;i < n;i++){
+  for(i = 0;i < n;i++) {
+    for(j = 0;j < '~';j++)
+      cost[j] = 0;
+
     totalCost = 0;
     scanf("%d%*c", &k);
     for(j = 0;j < k;j++) {
-      scanf("%c%*[^0-9]", &c);
-      scanf("%d%*[^\n]%*[\n]", &cost[c - 1]);
-      printf("%c(%d): %d\n", c, c, cost[c - 1]);
+      scanf("%c%*[ ]", &c);
+      scanf("%d", &cost[c - 1]);
+
+      while(getchar() != '\n');
     }
 
     scanf("%d%*c", &m);
     for(j = 0;j < m;j++) {
       getline(&line, &bufsize, stdin);
-      // printf("%s\n", line);
       for(l = 0;line[l] != '\n';l++) {
         totalCost += cost[line[l] - 1];
-        // printf("%c(%d): %d\n", line[l], line[l], cost[line[l] - 1]);
       }
     }
 
