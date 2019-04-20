312ad4bfb7a412dd52d5c49b609c9f083dea3d24
taira120 <taira1999120@gmail.com>
2019-04-21 04:25:19 +0900

    2つ目のテストから動かない(多分ラインバッファ周り

--- 2/11340.c
+++ 2/11340.c
@@ -7,7 +7,7 @@ int main() {
   int totalCost;
   int i, j, l;
   char c;
-  int cost['~' - ' '];
+  int cost['~'];
   char *line;
   size_t bufsize = 10000;
   line = (char *)malloc(bufsize * sizeof(char));
@@ -16,20 +16,23 @@ int main() {
     cost[i] = 0;
 
   scanf("%d", &n);
+  // while(1) {
   for(i = 0;i < n;i++){
     totalCost = 0;
     scanf("%d%*c", &k);
     for(j = 0;j < k;j++) {
       scanf("%c%*[^0-9]", &c);
-      scanf("%d%*c", &cost[c - 1]);
+      scanf("%d%*[^\n]%*[\n]", &cost[c - 1]);
+      printf("%c(%d): %d\n", c, c, cost[c - 1]);
     }
 
     scanf("%d%*c", &m);
     for(j = 0;j < m;j++) {
       getline(&line, &bufsize, stdin);
-      printf("%s\n", line);
+      // printf("%s\n", line);
       for(l = 0;line[l] != '\n';l++) {
         totalCost += cost[line[l] - 1];
+        // printf("%c(%d): %d\n", line[l], line[l], cost[line[l] - 1]);
       }
     }
 
