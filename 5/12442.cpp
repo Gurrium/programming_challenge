b0cbfb3a2e357edd639abdbe14d2e3831035e854
taira120 <taira1999120@gmail.com>
2019-05-21 11:09:23 +0900

    12442

--- 5/12442.cpp
+++ 5/12442.cpp
@@ -4,24 +4,19 @@
 
 using namespace std;
 
-int results[50000];
+vector<int> results, graph;
+vector<bool> visited;
 int n;
 
-int dfs(int root, vector<vector<int> > graph) {
-  if(results[root]) return results[root];
-  results[root] = 1;
-  int max_count = 0;
-
-  for(int i = 0;i < n;i++) {
-    if(graph[root][i]) {
-      int count = dfs(i, graph);
-      if(max_count < count) {
-        max_count = count;
-      }
-    }
+int dfs(int node) {
+  results[node] = 1;
+  visited[node] = true;
+  if(graph[node] != -1 && !visited[graph[node]]) {
+    results[node] += dfs(graph[node]);
   }
+  visited[node] = false;
 
-  return results[root] = max_count + 1;
+  return results[node];
 }
 
 int main() {
@@ -33,19 +28,22 @@ int main() {
     ans = max_count = 0;
 
     cin >> n;
-    vector<vector<int> > graph(n, vector<int>(n, 0));
+    graph.assign(n, -1);
+    results.assign(n, -1);
+    visited.assign(n, false);
     for(int j = 0;j < n;j++) {
       int u, v;
       cin >> u >> v;
-      graph[u - 1][v - 1] = 1;
+      graph[u - 1] = v - 1;
     }
 
     for(int j = 0;j < n;j++) {
-      memset(results, 0, sizeof(results));
-      int count = dfs(j, graph);
-      if(max_count < count) {
-        max_count = count;
-        ans = j;
+      if(results[j] == -1) {
+        int count = dfs(j);
+        if(max_count < count) {
+          max_count = count;
+          ans = j;
+        }
       }
     }
 
